bde4f99d74e250a0ab0cd0cc72529fb0437aac78
Harlan Heath <HarlanDHeath@gmail.com>
2018-07-04 15:02:43 -0500

    completed

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import ClassList from "./components/ClassList/ClassList";
 import Student from "./components/Student/Student";
+import History from "./components/History/History";
+import Contact from "./components/Contact/Contact";
 
 export default (
   <Switch>
     <Route component={Home} exact path="/" />
-    <Route component={About} exact path="/about" />
+    <Route component={About} path="/about" />
     <Route component={ClassList} path="/classlist/:class" />
     <Route component={Student} path="/student/:id" />
+    <Route component={History} path="/about/history" />
+    <Route component={Contact} path="/about/contact" />
   </Switch>
 );
